5290f08d8bbaa95d1621e43e9af91b064f5d4494
Daniel Xu <niellxu@yahoo.ca>
2022-03-19 16:02:26 -0400

    Format code

--- sklearn/model_selection/tests/test_split_new.py
+++ sklearn/model_selection/tests/test_split_new.py
@@ -1,18 +1,12 @@
-from unittest import expectedFailure
 import numpy as np
-import warnings
-import pytest
-
 from sklearn.model_selection._split import TimeSeriesSlidingWindow
-
-
 
 
 def test_default_behavior():
     X = np.arange(10)
     tscv = TimeSeriesSlidingWindow()
     splits = tscv.split(X)
-    
+
     train, test = next(splits)
     assert (train == np.array([0, 1])).all()
     assert (test == np.array([2, 3])).all()
@@ -51,7 +45,7 @@ def test_specify_train_size():
     X = np.arange(10)
     tscv = TimeSeriesSlidingWindow(train_size=4)
     splits = tscv.split(X)
-    
+
     train, test = next(splits)
     assert (train == np.array([0, 1, 2, 3])).all()
     assert (test == np.array([4, 5])).all()
@@ -59,12 +53,12 @@ def test_specify_train_size():
     train, test = next(splits)
     assert (train == np.array([1, 2, 3, 4])).all()
     assert (test == np.array([5, 6])).all()
-    
+
 
     train, test = next(splits)
     assert (train == np.array([2, 3, 4, 5])).all()
     assert (test == np.array([6, 7])).all()
-    
+
     train, test = next(splits)
     assert (train == np.array([3, 4, 5, 6])).all()
     assert (test == np.array([7, 8])).all()
@@ -83,7 +77,7 @@ def test_specify_test_size():
     X = np.arange(10)
     tscv = TimeSeriesSlidingWindow(test_size=4)
     splits = tscv.split(X)
-    
+
     train, test = next(splits)
     assert (train == np.array([0, 1])).all()
     assert (test == np.array([2, 3, 4, 5])).all()
@@ -91,12 +85,12 @@ def test_specify_test_size():
     train, test = next(splits)
     assert (train == np.array([1, 2])).all()
     assert (test == np.array([3, 4, 5, 6])).all()
-    
+
 
     train, test = next(splits)
     assert (train == np.array([2, 3])).all()
     assert (test == np.array([4, 5, 6, 7])).all()
-    
+
     train, test = next(splits)
     assert (train == np.array([3, 4])).all()
     assert (test == np.array([5, 6, 7, 8])).all()
@@ -115,7 +109,7 @@ def test_specify_gap():
     X = np.arange(10)
     tscv = TimeSeriesSlidingWindow(gap=2)
     splits = tscv.split(X)
-    
+
     train, test = next(splits)
     assert (train == np.array([0, 1])).all()
     assert (test == np.array([4, 5])).all()
@@ -147,7 +141,7 @@ def test_different_combination():
     X = np.arange(10)
     tscv = TimeSeriesSlidingWindow(train_size=3, test_size=3, gap=2)
     splits = tscv.split(X)
-    
+
     train, test = next(splits)
     assert (train == np.array([0, 1, 2])).all()
     assert (test == np.array([5, 6, 7])).all()
